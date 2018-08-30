2e7795f98ebb67558327749e6e4b49ed3ac7e105
omar <galvez_soto@hotmail.com}>
2018-08-30 11:11:57 -0600

    Agregar archivos de proyecto.

---
+++ demoGIT from vs/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demoGIT_from_vs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
